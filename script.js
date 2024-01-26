fa3938c2cc03c881ca885ecefcc02b98c3f15825
KimPlacha <155601464+KimPlacha@users.noreply.github.com>
2024-01-26 00:26:12 -0800

    test again

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Script");
+
+var userName="George";// creating and assigning value to the var
+
+userName = "James"; // changing the var value
+
+console.log(userName);
+
+var employeeName="Kory";
+var employeeAge = 27;
+var employeeCountry = "USA"
+var employeePhone="8052639449";
+var employeeSalaryMonthly=20000;
+var employeeSalaryYearly= employeeSalaryMonthly *12
+
+document.write(
+
+    <><h3>Employee Info</h3><p>Name: $(employeeName)</p><p>Age: $(employeeAge)</p></>
+
+);
