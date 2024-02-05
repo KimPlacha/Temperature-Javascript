e9566e0d8693cfc1061efbe9616b9d8d1017fc4d
KimPlacha <155601464+KimPlacha@users.noreply.github.com>
2024-02-05 03:49:52 -0800

    final (draft)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 function convertTemperature() {
     let temperature = (document.getElementById('inputTemperature').value);
     let scale = document.getElementById('scale').value;
+
     let result;
     if (scale === 'celsius') {
         result = (temperature - 32) * (5/9);
@@ -11,21 +12,16 @@ function convertTemperature() {
     document.getElementById('result').innerHTML = result.toFixed(2) + 'º' + (scale === 'celsius' ? 'Celsius' : 'Fahrenheit');
 }
 
-function convertTemperatureRange(){
-    let startValue = (document.getElementById("endValue").value);
-    let endValue = (document.getElementById("endValue").value);
-    let scale = document.getElementById("scale").value;
-    let result = document.getElementById("result");
+function convertTemperatureRange ();
+    let temperature = (document.getElementById('startValue').value);
+    let temperature = (document.getElementById('endValue').value);
+    let scale = document.getElementById('scale').value;
 
-for (let temperature = startValue; temperature <= endValue; temperature += 5) { 
-    let convertTemperatureRange
-    if (scale === "celsius") {
-        let convertTemperatureRange = (temperature * 9/5) + 32;
-
-for (let temperature = startValue; temperature <= endValue; temperature + 5) {
-    let convertTemperatureRange
-    if (scale === "fahrenheit") {
-        let convertTemperature = (temperature - 32) * 5/9;
-} else {
-    result = "Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.";
-}
+    let result;
+    if (scale === 'celsius') {
+        result = (temperature - 32) * (5/9);
+    } else {
+        result = (temperature * 9/5) + 32;
+    }
+    
+    document.getElementById('result').innerHTML = result.toFixed(2) + 'º' + (scale === 'celsius' ? 'Celsius' : 'Fahrenheit');
